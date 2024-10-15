a13fd078151abbe41f72c7eee706b6368e6b1c1c
Maggie <bertzero28@gmail.com>
2024-10-15 10:27:36 +0800

    更新

--- src/view/sys_common/login.vue
+++ src/view/sys_common/login.vue
@@ -98,7 +98,7 @@
             >Create an account</a>
           </p>
 
-          <p class="text-sm text-gray-500 mt-12">&copy;范成磊团队 2024 MYing实验室</p>
+          <p class="text-sm text-gray-500 mt-12">&copy;范成磊团队人员 2024 MYing实验室</p>
         </el-form>
       </div>
     </div>
